c16160c1cffba58de454a373872a2854843c530c
KamalaDone <donekamala@gmail.com>
2024-12-20 12:23:36 +0530

    upgrad ai.pkl

--- app.py
+++ app.py
@@ -9,19 +9,19 @@ def home():
 @api.route('/predict',methods=['GET'])
 def predict():
     N=request.args.get('N')
-    N=float('N')
+    N=float(N)
     P=request.args.get('P')
-    P=float('P')
+    P=float(P)
     K=request.args.get('K')
-    K=float('K')
+    K=float(K)
     T=request.args.get('T')
-    T=float('T')
+    T=float(T)
     H=request.args.get('H')
-    H=float('H')
+    H=float(H)
     PH=request.args.get('PH')
-    PH=float('PH')
+    PH=float(PH)
     R=request.args.get('R')
-    R=float('R')
+    R=float(R)
     data=[[N,P,K,T,H,PH,R]]
     response=ai.predict(data)[0]
     return response
